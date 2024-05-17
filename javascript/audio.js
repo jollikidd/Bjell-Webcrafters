ca5a9b12bb7347365ddd2f7937dd54a78d99cb05
William Reid <dubjayreid@gmail.com>
2024-05-17 22:54:05 +1200

    fixed the sheepy sounds

--- javascript/audio.js
+++ javascript/audio.js
@@ -1,9 +1,14 @@
 "use strict";
 
 const sheepImg1 = document.getElementById("sheepImg1");
+const sheepImg2 = document.getElementById("sheepImg2");
 
-const audio = new Audio("./audio/ Sheep Sound 1.mp3");
-
+const audio = new Audio("./audio/Sheep Sound 1.mp3");
+const audio2 = new Audio("./audio/deep_sheep.mp3");
 sheepImg1.addEventListener("click", () => {
   audio.play();
+});
+
+sheepImg2.addEventListener("click", () => {
+  audio2.play();
 });
