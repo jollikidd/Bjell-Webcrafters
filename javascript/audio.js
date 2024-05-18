d00f5365a22e72269015344885acfe977e3ec9f9
kamuke <kerttukamula@gmail.com>
2024-05-18 12:40:00 +1200

    added audio files and click event to the other sheep svg

--- javascript/audio.js
+++ javascript/audio.js
@@ -1,9 +1,15 @@
-"use strict";
+'use strict';
 
-const sheepImg1 = document.getElementById("sheepImg1");
+const sheepImg1 = document.getElementById('sheepImg1');
+const audio1 = new Audio('./audio/Sheep_Sound_1.mp3');
 
-const audio = new Audio("./audio/ Sheep Sound 1.mp3");
+const sheepImg2 = document.getElementById('sheepImg2');
+const audio2 = new Audio('./audio/Sheep_Sound_2.mp3');
 
-sheepImg1.addEventListener("click", () => {
-  audio.play();
+sheepImg1.addEventListener('click', () => {
+  audio1.play();
+});
+
+sheepImg2.addEventListener('click', () => {
+  audio2.play();
 });
